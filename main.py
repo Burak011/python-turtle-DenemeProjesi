3df8de059dcd7c93e18a6f744ac68794b4789f11
Doğukan Burak Çelikten <dcelikten01@icloud.com>
2025-05-30 18:01:22 +0300

    main.py eklendi

---
+++ main.py
@@ -0,0 +1,12 @@
+import turtle
+
+drawing_board = turtle.Screen()
+drawing_board.bgcolor("green")
+drawing_board.title("Turtle Drawing Board")
+
+turtle_instance = turtle.Turtle()
+turtle_instance.forward(100)
+turtle_instance.pencolor("white")
+
+
+turtle.done()
